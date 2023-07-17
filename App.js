b6ba218258a20707da887f4d516d0ca00f981d38
bekirbali <bfbali43@gmail.com>
2023-07-17 03:33:14 +0300

    starting to develop

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [items, setItems] = useState([1, 2, 3]);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <Button title="tap me!" />
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Your goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        {items?.map((item) => (
+          <Text> {item},</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 50,
   },
-  text: {
-    color: "red",
-    fontSize: 48,
-    borderColor: "blue",
-    padding: 10,
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  input: {
     borderWidth: 2,
-    marginBottom: 10,
+    borderColor: "#f0c",
+    width: "80%",
+    padding: 8,
   },
 });
