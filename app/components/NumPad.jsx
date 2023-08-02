53ff71aa63dab65ba6f7666454b3da22a2a9aa2b
bekirbali <bfbali43@gmail.com>
2023-08-02 21:12:54 +0300

    new look

--- app/components/NumPad.jsx
+++ app/components/NumPad.jsx
@@ -36,34 +36,71 @@ const NumPad = () => {
 
   const [text, setText] = useState("");
 
-  const Btn = ({ title }) => (
-    <TouchableOpacity
-      style={styles.opacity}
-      onPress={() => calculateHandler(title)}
-    >
-      <Text style={styles.text}>{title}</Text>
-    </TouchableOpacity>
-  );
   return (
     <View style={styles.numpadContainer}>
-      {/* <Btn title="AC" />
-      <Btn title="1" />
-      <Btn title="2" />
-      <Btn title="3" />
-      <Btn title="4" />
-      <Btn title="5" />
-    <Btn title="7" /> */}
-      <View style={styles.pad}>
-        {arr.map((item, index) => (
-          <Pressable
-            key={index}
-            onPress={() => calculateHandler(item)}
-            style={styles.text}
-          >
-            {item}
-          </Pressable>
-        ))}
-      </View>
+      <>
+        <div className="line-clamp">
+          <h1>line clamp test</h1>
+          <div className="inner">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+              blanditiis corporis nihil ipsam harum amet. Debitis nemo illo eius
+              expedita molestias soluta totam. Delectus quos excepturi id minus!
+              Quam harum autem ratione tempora debitis laborum exercitationem
+              odio excepturi laudantium, voluptate eum aut cumque unde tenetur
+              fugiat incidunt, doloribus repudiandae?
+            </p>
+            <h2>26</h2>
+          </div>
+        </div>
+        <div className="calculator">
+          <div className="inputArea">
+            <input
+              type="text"
+              value={text}
+              onChange={changeHandler}
+              className="inputTop"
+            />
+            <input
+              type="text"
+              value={text}
+              onChange={changeHandler}
+              className="inputBottom"
+            />
+          </div>
+          <div className="padArea">
+            <div className="first row">
+              <p onClick={clickHandler}>AC</p>
+              <p onClick={clickHandler}>DEL</p>
+              <p onClick={operandHandler}>%</p>
+              <p onClick={operandHandler}>/</p>
+            </div>
+            <div className="second row">
+              <p onClick={clickHandler}>7</p>
+              <p onClick={clickHandler}>8</p>
+              <p onClick={clickHandler}>9</p>
+              <p onClick={operandHandler}>*</p>
+            </div>
+            <div className="third row">
+              <p onClick={clickHandler}>4</p>
+              <p onClick={clickHandler}>5</p>
+              <p onClick={clickHandler}>6</p>
+              <p onClick={operandHandler}>-</p>
+            </div>
+            <div className="forth row">
+              <p onClick={clickHandler}>1</p>
+              <p onClick={clickHandler}>2</p>
+              <p onClick={clickHandler}>3</p>
+              <p onClick={operandHandler}>+</p>
+            </div>
+            <div className="fifth row">
+              <p onClick={clickHandler}>0</p>
+              <p onClick={operandHandler}>.</p>
+              <p onClick={operandHandler}>=</p>
+            </div>
+          </div>
+        </div>
+      </>
     </View>
   );
 };
